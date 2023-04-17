2204cc1c58504f271bab65c4d4966765ba08f285
Skyflexx <118121459+Skyflexx@users.noreply.github.com>
2023-04-17 11:57:38 +0200

    File organisation

---
+++ livres/Author.php
@@ -0,0 +1,60 @@
+<?php
+
+// Classe Auteur 
+
+    class Author{
+
+        private string $firstname;
+        private string $lastname;
+        private array $bookList;
+
+        public function __construct($firstname, $lastname){
+            $this->firstname = $firstname;
+            $this->lastname = $lastname;
+            $this->bookList = array();
+        }
+
+        public function getFirstname(){
+            return $this->firstname;
+        }
+
+        public function getLastname(){
+            return $this->lastname;
+        }
+
+        public function setFirstname($newFirstname){
+            $this->firstname = $newFirstname;
+        }
+
+        public function setLastname($newLastname){
+            $this->lastname = $newLastname;            
+        }
+
+        public function addBook(Book $book){ // fonction automatiquement appelée lors de la création d'un new Book puisque lié à un auteur.
+
+            array_push($this->bookList, $book); // On ajoute dans l'array l'objet Book créé. $this->bookList étant l'array.
+
+        }
+
+        public function showBibliography(){
+
+            $bibliography = "Livres de $this->firstname $this->lastname :<br>";
+
+            foreach ($this->bookList as $book){
+
+                $bibliography = $bibliography.$book->getInfoBook();               
+                
+            }
+
+            return $bibliography;  
+        }
+
+        public function __tostring(){
+
+            return $this->getfirstName()." ".$this->getLastname();       
+            
+        }    
+
+    }
+
+?>
